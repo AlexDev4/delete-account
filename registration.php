c1dbf3682746bd4f092650e620be990c622e8512
Alexis Olive <alexis-olive@live.fr>
2023-08-06 16:13:47 +0200

    [FIX] - Change namespace from AOL to Histomagento

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AOL_DeletingAccount',
+    'Histomagento_DeletingAccount',
     __DIR__
 );
